2c2b40c204ab51358c7d0c26579fe034a8876e57
myfender <myfender@tiscali.it>
2020-07-27 15:35:40 +0200

    2 files modified

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'Pippobe'
+      name: 'Pippo'
     };
   }
 
@@ -16,7 +16,7 @@ class App extends Component {
       <div>
         <Hello name={this.state.name} />
         <p>
-          Start editing to see some magic happen :)
+          Proviamo a editare il file in real time :)
         </p>
       </div>
     );
